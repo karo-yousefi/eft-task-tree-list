b06af576e2be3e0c0b481d6a09395c68074b18d5
Karo Yousefi <karoyousefi.80@gmail.com>
2024-05-16 12:28:21 +0330

    update two

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,64 @@ const settingTwoButton = document.getElementById("setting-two");
 const settingTwoIcon = document.getElementById("setting-two-icon");
 let settingTwo = false;
 
+const settingThreeButton = document.getElementById("setting-three");
+const settingThreeIcon = document.getElementById("setting-three-icon");
+let settingThree = false;
 
-function settingOneTurnOn(){
-    if(settingOne === true){
-        settingOneIcon.style.color = "white";
-        settingOne = false;
+const taskBoxIconOne = document.getElementById("icon-one");
+const taskBoxIconTwo = document.getElementById("icon-two");
+const taskBoxIconThree = document.getElementById("icon-three");
+
+
+function settingTurnOn(button){
+    if(button === 0){
+        if(settingOne === true){
+            settingOneIcon.style.color = "white";
+            settingOneIcon.style.textDecoration = "none";
+            // Diactivating the function needed for setting one HERE
+            taskBoxIconOne.style.opacity = "0";
+            settingOne = false;
+        }
+        else{
+            settingOneIcon.style.color = "lime"
+            settingOneIcon.style.textDecoration = "underline";
+            // Activating the function needed for setting one HERE
+            taskBoxIconOne.style.opacity = "1";
+            settingOne = true;
+        }
+
     }
-    else{
-        settingOneIcon.style.color = "lime"
-        settingOne = true;
+    else if (button === 1){
+        if(settingTwo === true){
+            settingTwoIcon.style.color = "white";
+            settingTwoIcon.style.textDecoration = "none";
+            // Diacptivating the function needed for setting two HERE
+            taskBoxIconTwo.style.opacity = 0;
+            settingTwo = false;
+        }
+        else{
+            settingTwoIcon.style.color = "red"
+            settingTwoIcon.style.textDecoration = "underline";
+            // Activating the function needed for setting two HERE
+            taskBoxIconTwo.style.opacity = 1;
+            settingTwo = true;
+        }
+    }
+    else if (button === 2){
+        if(settingThree === true){
+            settingThreeIcon.style.color = "white";
+            settingThreeIcon.style.textDecoration = "none";
+            // Diacptivating the function needed for setting two HERE
+            taskBoxIconThree.style.opacity = 0;
+            settingThree = false;
+        }
+        else{
+            settingThreeIcon.style.color = "yellow"
+            settingThreeIcon.style.textDecoration = "underline";
+            // Activating the function needed for setting two HERE
+            taskBoxIconThree.style.opacity = 1;
+            settingThree = true;
+        }
     }
 }
 
@@ -30,6 +79,8 @@ function settingTwoTurnOn(){
     }
 }
 
-settingOneButton.addEventListener("click", settingOneTurnOn);
-settingTwoButton.addEventListener("click", settingTwoTurnOn);
+settingOneButton.addEventListener("click", () => settingTurnOn(0));
+settingTwoButton.addEventListener("click", () => settingTurnOn(1));
+settingThreeButton.addEventListener("click", () => settingTurnOn(2));
+
 
